a03647c2c3bdc7cf7651265da68d2ed14e25bbe8
Jim Rudolf <rudolf@jbrcodes.com>
2020-09-30 11:48:40 +0200

    More small stuff...

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
-const createError = require('http-errors');
 const { ensureSameUser } = require('../middleware/guards');
 const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require('../config');
 const db = require("../model/helper");
 
+
+/**
+ * Register a user
+ **/
 
 router.post('/register', async (req, res, next) => {
     let { username, password, email } = req.body;
@@ -25,26 +28,32 @@ router.post('/register', async (req, res, next) => {
 });
 
 
+/**
+ * Log in a user
+ **/
+
 router.post('/login', async (req, res, next) => {
     let { username, password } = req.body;
 
     try {
         let results = await db(`SELECT * FROM users WHERE username = '${username}'`);
         if (results.data.length === 0) {
+            // Username not found
             res.status(400).send({ error: 'Login failed' });
         } else {
-            let row = results.data[0];
+            let row = results.data[0];  // the user's row/record from the DB
             let passwordsEqual = await bcrypt.compare(password, row.password);
             if (passwordsEqual) {
                 let payload = { userId: row.id };
+                // Create & return token (and user ID, useful for the client)
                 let token = jwt.sign(payload, SECRET_KEY);
-                // Return the token and some other useful stuff
                 res.send({
                     message: 'Login succeeded',
                     token: token,
                     userId: row.id
                 });
             } else {
+                // Passwords don't match
                 res.status(400).send({ error: 'Login failed' });
             }
         }
@@ -70,7 +79,8 @@ router.get('/', async function(req, res, next) {
 
 
 /**
- * Get the user's profile page
+ * Get the user's profile page.
+ * A user can only see his/her own profile page.
  **/
 
 router.get('/:userId/profile', ensureSameUser, async function(req, res, next) {
@@ -79,12 +89,11 @@ router.get('/:userId/profile', ensureSameUser, async function(req, res, next) {
     try {
         let results = await db(sql);
         let user = results.data[0];
-        delete user.password;
+        delete user.password;  // don't return the password
         res.send(user);
     } catch (err) {
         next(err);
     }
-
 });
 
 

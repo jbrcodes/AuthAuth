3c8dde0537fd696817048d18f74db7c80f594bc5
Jim Rudolf <rudolf@jbrcodes.com>
2022-03-02 23:04:13 +0100

    Upgrade to RR v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
 import Local from './helpers/Local';
 import Api from './helpers/Api';
 
 import NavBar from './components/NavBar';
-import Routes from './components/Routes';
+
+import PrivateRoute from './components/PrivateRoute';
+import LoginView from './views/LoginView';
+import ErrorView from './views/ErrorView';
+import MembersOnlyView from './views/MembersOnlyView';
+import ProfileView from './views/ProfileView';
+import UsersView from './views/UsersView';
 
 
 function App() {
     const [user, setUser] = useState(Local.getUser());
     const [loginErrorMsg, setLoginErrorMsg] = useState('');
-    const history = useHistory();
+    const navigate = useNavigate();
 
     async function doLogin(username, password) {
         let response = await Api.loginUser(username, password);
@@ -20,7 +26,7 @@ function App() {
             Local.saveUserInfo(response.data.token, response.data.user);
             setUser(response.data.user);
             setLoginErrorMsg('');
-            history.push('/');
+            navigate('/');
         } else {
             setLoginErrorMsg('Login failed');
         }
@@ -29,7 +35,7 @@ function App() {
     function doLogout() {
         Local.removeUserInfo();
         setUser(null);
-        history.push('/');
+        navigate('/');
     }
 
     return (
@@ -37,10 +43,27 @@ function App() {
             <NavBar user={user} logoutCb={doLogout} />
 
             <div className="container">
-                <Routes
-                    loginCb={(u, p) => doLogin(u, p)}
-                    loginError={loginErrorMsg}
-                />
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>} />
+                    <Route path="/users" element={<UsersView />} />
+                    <Route path="/users/:userId" element={
+                        <PrivateRoute>
+                            <ProfileView />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/members-only" element={
+                        <PrivateRoute>
+                            <MembersOnlyView />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/login" element={
+                        <LoginView 
+                            loginCb={(u, p) => doLogin(u, p)} 
+                            loginError={loginErrorMsg} 
+                        />
+                    } />
+                    <Route path="*" element={<ErrorView code="404" text="Page not found" />} />
+                </Routes>
             </div>
         </div>
     );
